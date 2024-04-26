735ecbffed15c727276feace505c0ab49c9f6dd1
youjinkim <kelly2.youjin.kim@gmail.com>
2024-04-26 12:42:17 +0900

    modify

--- seleniums/persona1_navercafe_guillainbarre.py
+++ seleniums/persona1_navercafe_guillainbarre.py
@@ -49,24 +49,50 @@ time.sleep(2)
 browser.switch_to.frame('cafe_main') #프레임 전환
 time.sleep(2)
 
-browser.find_element(by=By.CSS_SELECTOR, value='#main-area > div:nth-child(4) > table > tbody > tr:nth-child(1) > td.td_article > div.board-list > div > a').click() #게시글 클릭
+browser.find_element(by=By.CSS_SELECTOR, value='#main-area > div:nth-child(4) > table > tbody > tr:nth-child(1) > td.td_article > div.board-list > div').click() #게시글 클릭
 time.sleep(2)
 
-
-for i in range(91):
-    try:
-        title=browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div.ArticleContentBox > div.article_header > div.ArticleTitle > div > h3').text #글제목 추출
-        content=browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div.ArticleContentBox > div.article_container > div.article_viewer > div > div.content.CafeViewer > div > div').text #글내용 추출
-        
-        
-        print('<',i+1,'번째 글> : ',title)
-        print(content)
-        print('')
-
-        browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div.ArticleTopBtns > div.right_area > a.BaseButton.btn_next.BaseButton--skinGray.size_default').click() #다음글 버튼 클릭
-        time.sleep(5)
-    except :
-        browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div > div.guide_btns > a:nth-child(2) > span').click() #다음글 버튼 클릭
-
     
+while True:
+    try :
+        time.sleep(2)
+        title=browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div.ArticleContentBox > div.article_header > div.ArticleTitle > div > h3').text # 글 제목 추출
+        name=browser.find_element(by=By.CSS_SELECTOR, value='.nickname').text #작성자 추출
+        date=browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div.ArticleContentBox > div.article_header > div.WriterInfo > div.profile_area > div.article_info > span.date').text #작성일 추출
+        contents=browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div.ArticleContentBox > div.article_container > div.article_viewer > div > div.content.CafeViewer > div > div').text #글내용 추출
+        num = browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div.ArticleContentBox > div.article_container > div.ReplyBox > div.box_left > a > strong').text # 댓글 개수
+        num = int(num)
+        if num != 0:
+            reply_list = []
+            for j in range(num): 
+                reply = browser.find_element(by=By.XPATH, value=f'/html/body/div/div/div/div[2]/div[2]/div[5]/ul/li[{j+1}]/div/div/div[2]/p/span').text
+                reply_list.append(reply)
+        else : 
+            reply_list = ""
+
+        print(reply_list)
+                
+    except :
+        pass
+
+    data={
+    'cafe' : '길랑바레, 밀러피셔 증후군 카페',  # 카페 이름
+    'title' : title,
+    'name' : name,
+    'date' : date,
+    'contents' : contents,
+    'review' : reply_list
+    }
+
+    print(data)
+    try :
+        browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div.ArticleTopBtns > div.right_area > a.BaseButton.btn_next.BaseButton--skinGray.size_default').click() #다음글 버튼 클릭
+    except :
+        browser.find_element(by=By.CSS_SELECTOR, value='#app > div > div > div > div.guide_btns > a:nth-child(2) > span').click() # 회원 등급 제한 걸릴 시 '다음글 보기'버튼 클릭
+    time.sleep(2)
+    
+
+    collection.insert_one(data)
+
+
 browser.close()
